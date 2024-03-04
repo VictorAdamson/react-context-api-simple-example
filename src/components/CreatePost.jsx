4875932f9fe4e15785af302e1c655497682996ca
Victor <victor.adamson@se.experis.com>
2024-03-04 12:36:25 +0100

    Complete

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,40 +1,66 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { AppContext } from "../App";
 
 const INITIAL_POST = {
-    title: '',
-    content: '',
-}
+  title: "",
+  content: "",
+};
 
-export default function CreatePost({ posts, setPosts }) {
-    const [post, setPost] = useState(INITIAL_POST)
+const getPostFromLocalStorage = () => {
+  const postFromLocalStorage = localStorage.getItem("wipPost");
+  if (postFromLocalStorage !== null && postFromLocalStorage !== undefined) {
+    console.log("Loading data from local storage", postFromLocalStorage);
+    return JSON.parse(postFromLocalStorage);
+  } else {
+    console.log("Loading inital data");
+    return { ...INITIAL_POST };
+  }
+};
+export default function CreatePost() {
+  const context = useContext(AppContext);
+  const [post, setPost] = useState(getPostFromLocalStorage());
 
-    const handleChange = (e) => {
-        const { name, value } = e.target
-        setPost({
-          ...post,
-          [name]: value,
-        })
-    }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const setWipPost = {
+      ...post,
+      [name]: value,
+    };
+    setPost(setWipPost);
+    localStorage.setItem("wipPost", JSON.stringify(setWipPost));
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        setPosts([...posts, post])
-        setPost(INITIAL_POST)
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    context.setPosts([...context.posts, post]);
+    setPost(INITIAL_POST);
+    localStorage.removeItem("wipPost");
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label>
-              Title:
-              <input type="text" name="title" onChange={handleChange} value={post.title}></input>
-            </label>
-            <br/>
-            <label>
-              Content:
-              <textarea name="content" onChange={handleChange} value={post.content} cols={50} rows={5}></textarea>
-            </label>
-            <br/>
-            <input type="submit" value="Post!"></input>
-        </form>
-    )
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Title:
+        <input
+          type="text"
+          name="title"
+          onChange={handleChange}
+          value={post.title}
+        ></input>
+      </label>
+      <br />
+      <label>
+        Content:
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={post.content}
+          cols={50}
+          rows={5}
+        ></textarea>
+      </label>
+      <br />
+      <input type="submit" value="Post!"></input>
+    </form>
+  );
 }
